682d45d06cdbc0301e6ef7e93580a991a37238d7
Jinhee Yang <78382611+Jina-123@users.noreply.github.com>
2021-05-29 17:07:03 +0200

    Create paiement.js

---
+++ paiement.js
@@ -0,0 +1,45 @@
+<script src="paiement.js"></script>
+
+<script type="text/javascript">
+
+$(document).ready(function(e) {
+
+var total = 55 + 42;
+
+$('.add_cart > button').click(function(e) {
+
+var btnName =  $(this).get(0).className;
+
+switch (btnName) {
+
+case "btn_add1":
+
+console.log("Ajouter vos articles");
+
+break;
+
+case "btn_add2":
+
+console.log("Ajouter vos articles");
+
+break;
+
+case "btn_add3":
+
+console.log("Ajouter vos articles");
+
+break;
+
+case "btn_add4":
+
+console.log("Ajouter vos articles");
+
+break;
+
+}
+
+});
+
+});
+
+</script>
